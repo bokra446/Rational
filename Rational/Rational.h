747ae79e92be175d7c8cbd2650b19cf6548874cd
bokra <katja44667@gmail.com>
2021-02-25 21:54:40 +0300

    Добавьте файлы проекта.

---
+++ Rational/Rational.h
@@ -0,0 +1,62 @@
+#pragma once
+
+/**
+ * Дробь, x/y
+ */
+class Rational {
+public:
+    // not a number
+    // Указывает, что число не является числом
+    // по аналогии с float
+    // Устанавливается в true,
+    // если выполнили операцию
+    // с делением на ноль
+    bool nan;
+    int numerator;
+    int denumerator;
+
+    Rational();
+    Rational(int, int);
+    Rational(int);
+
+    // Сокращение
+    Rational reduce();
+
+    // *-1
+    Rational neg();
+    // ^-1
+    Rational inv();
+
+    Rational sum(Rational);
+    Rational sub(Rational);
+    Rational mul(Rational);
+    Rational div(Rational);
+
+    // ==
+    bool eq(Rational);
+    // !=
+    bool neq(Rational);
+    // less than
+    // <
+    bool l(Rational);
+    // less or equal than
+    // <=
+    bool leq(Rational);
+    // greater than
+    // >
+    bool g(Rational);
+    // greater or equal than
+    // >=
+    bool geq(Rational);
+
+    // Вывести на экран
+    void print();
+    // Считать с клавиатуры
+    void scan();
+
+    //Перегрузка операторов
+    Rational operator+ (Rational);
+    Rational operator- (Rational);
+    Rational operator* (Rational);
+    Rational operator/ (Rational);
+};
